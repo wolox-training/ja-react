c8dc4805b98f4f8b0970da8dd892c486ae1ff23b
Julian alvarez <alvarezjulianignacio@gmail.com>
2019-06-11 15:23:37 -0300

    square function component

--- react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -3,17 +3,16 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button type="button"
-        className={styles.square}
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    )
-  }
+function Square (props) {
+  return (
+    <button
+      type="button"
+      className={styles.square}
+      onClick={() => props.onClick()}
+    >
+      {props.value}
+    </button>
+  )
 }
 
 export default Square;
